5dcbf1b8fbddb32adf68fd80b895489cd03707f4
xiangyw99 <xiangyw99@outlook.com>
2024-02-17 23:33:43 +0800

    v1

--- app.py
+++ app.py
@@ -1,20 +1,110 @@
-import streamlit as st
-from transformers import pipeline
-from PIL import Image
 
-pipeline = pipeline(task="image-classification", model="julien-c/hotdog-not-hotdog")
+from langchain.vectorstores import Chroma
+from langchain.embeddings.huggingface import HuggingFaceEmbeddings
+import os
+from LLM import InternLM_LLM
+from langchain.prompts import PromptTemplate
+from langchain.chains import RetrievalQA
+from openxlab.model import download
 
-st.title("Hot Dog? Or Not?")
+download(model_repo='OpenLMLab/internlm-chat-7b', 
+        model_name='InternLM-7b', output='./model/InternLM')
+os.environ['HF_ENDPOINT'] = 'https://hf-mirror.com'
 
-file_name = st.file_uploader("Upload a hot dog candidate image")
+os.system('huggingface-cli download --resume-download sentence-transformers/paraphrase-multilingual-MiniLM-L12-v2 --local-dir ./model/sentence-transformer')
 
-if file_name is not None:
-    col1, col2 = st.columns(2)
 
-    image = Image.open(file_name)
-    col1.image(image, use_column_width=True)
-    predictions = pipeline(image)
+def load_chain():
+    # 加载问答链
+    # 定义 Embeddings
 
-    col2.header("Probabilities")
-    for p in predictions:
-        col2.subheader(f"{ p['label'] }: { round(p['score'] * 100, 1)}%")
+    embeddings = HuggingFaceEmbeddings(model_name="./model/sentence-transformer")
+
+    # 向量数据库持久化路径
+    persist_directory = './law_data_base/vector_db/chroma'
+
+    # 加载数据库
+    vectordb = Chroma(
+        persist_directory=persist_directory,  # 允许我们将persist_directory目录保存到磁盘上
+        embedding_function=embeddings
+    )
+
+    # 加载自定义 LLM
+    llm = InternLM_LLM(model_path = "./model/InternLM/internlm-chat-7b")
+
+    # 定义一个 Prompt Template
+    template = """使用以下上下文来回答和宪法有关的问题。如果你不知道答案，就说你不知道，不要试图编造答
+    案。尽量使答案简明扼要。总是在回答的最后说“谢谢你的提问！”。
+    {context}
+    问题: {question}
+    有用的回答:"""
+
+    QA_CHAIN_PROMPT = PromptTemplate(input_variables=["context","question"],template=template)
+
+    # 运行 chain
+    qa_chain = RetrievalQA.from_chain_type(llm,retriever=vectordb.as_retriever(),return_source_documents=True,chain_type_kwargs={"prompt":QA_CHAIN_PROMPT})
+    
+    return qa_chain
+
+class Model_center():
+    """
+    存储检索问答链的对象 
+    """
+    def __init__(self):
+        # 构造函数，加载检索问答链
+        self.chain = load_chain()
+
+    def qa_chain_self_answer(self, question: str, chat_history: list = []):
+        """
+        调用问答链进行回答
+        """
+        if question == None or len(question) < 1:
+            return "", chat_history
+        try:
+            chat_history.append(
+                (question, self.chain({"query": question})["result"]))
+            # 将问答结果直接附加到问答历史中，Gradio 会将其展示出来
+            return "", chat_history
+        except Exception as e:
+            return e, chat_history
+
+import gradio as gr
+
+# 实例化核心功能对象
+model_center = Model_center()
+# 创建一个 Web 界面
+block = gr.Blocks()
+with block as demo:
+    with gr.Row(equal_height=True):   
+        with gr.Column(scale=15):
+            # 展示的页面标题
+            gr.Markdown("""<h1><center>InternLM</center></h1>
+                <center>书生浦语·法律</center>
+                """)
+
+    with gr.Row():
+        with gr.Column(scale=4):
+            # 创建一个聊天机器人对象
+            chatbot = gr.Chatbot(height=450, show_copy_button=True)
+            # 创建一个文本框组件，用于输入 prompt。
+            msg = gr.Textbox(label="Prompt/问题")
+
+            with gr.Row():
+                # 创建提交按钮。
+                db_wo_his_btn = gr.Button("Chat")
+            with gr.Row():
+                # 创建一个清除按钮，用于清除聊天机器人组件的内容。
+                clear = gr.ClearButton(
+                    components=[chatbot], value="Clear console")
+                
+        # 设置按钮的点击事件。当点击时，调用上面定义的 qa_chain_self_answer 函数，并传入用户的消息和聊天历史记录，然后更新文本框和聊天机器人组件。
+        db_wo_his_btn.click(model_center.qa_chain_self_answer, inputs=[
+                            msg, chatbot], outputs=[msg, chatbot])
+
+    gr.Markdown("""提醒：<br>
+    1. 初始化数据库时间可能较长，请耐心等待。
+    2. 使用中如果出现异常，将会在文本输入框进行展示，请不要惊慌。 <br>
+    """)
+gr.close_all()
+# 直接启动
+demo.launch()
